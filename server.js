e7a382ed39329b28cbbf7099672fffa83f1c0ebb
Malamud-A <malamud.a98@gmail.com>
2018-04-18 15:20:06 +0300

    server setup

---
+++ server.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console*/
+const express = require('express');
+  app = express();
+  hostname = '0.0.0.0';
+  port = parseInt(process.env.PORT, 10) || 8080;
+  publicDir = __dirname + '/src';
+  path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicDir, '/index.html'));
+});
+
+app.use(express.static(publicDir));
+
+console.log('Listening at' + port);
+app.listen(port, hostname);
+
